d1d7ef1816b2df611f767ba3a91f796479dffd4e
Matheus <maatheus.santos223@gmail.com>
2023-11-14 15:44:52 -0300

    feat: add user-routes

--- src/server/App.ts
+++ src/server/App.ts
@@ -1,21 +1,22 @@
 import express from 'express';
+import UserRoutes from '../routes/user-routes';
 
 class App {
-    public express: express.Application;
+  public express: express.Application;
 
-    constructor() {
-        this.express = express();
-        this.middleware();
-        this.routes;
-    }
+  constructor() {
+    this.express = express();
+    this.middleware();
+    this.routes();
+  }
 
-    private middleware(): void {
-        this.express.use(express.json());
-    }
+  private middleware(): void {
+    this.express.use(express.json());
+  }
 
-    private routes(): void {
-        this.express.use();
-    }
+  private routes(): void {
+    this.express.use('/users', UserRoutes);
+  }
 }
 
 export default new App().express;
