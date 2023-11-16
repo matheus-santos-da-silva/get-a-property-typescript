58a942830a8239d27b661764b9cc69eb4e17acd7
Matheus <maatheus.santos223@gmail.com>
2023-11-16 13:52:56 -0300

    feat: changing user routes

---
+++ src/routes/user-routes.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import { UserController } from '../controllers/user-controller';
+import { validationMiddleware } from '../middleware/validation';
+import { createUserValidationSchema } from '../utils/users-validation-schemas/create-user-validation';
+import { LoginUserValidationSchema } from '../utils/users-validation-schemas/login-user-validation';
+
+const router = Router();
+
+router.post('/register', validationMiddleware(createUserValidationSchema), UserController.register);
+router.post('/login', validationMiddleware(LoginUserValidationSchema), UserController.login);
+
+export default router;
