5c16f79028417b22a505e27175217b49df4bf2e4
Matheus <maatheus.santos223@gmail.com>
2023-11-16 14:11:27 -0300

    fix: remove login that repository

--- src/repositories/users-repository.ts
+++ src/repositories/users-repository.ts
@@ -1,9 +1,6 @@
 import { User, UserProps } from '../entities/user';
-import { LoginUserRequest, LoginUserResponse } from '../use-cases/users/login-user';
-
 
 export interface UsersRepository {
   create(user: User): Promise<void>
-  findEmailConflicts(email: string): Promise<UserProps | null>
-  login(props: LoginUserRequest): Promise<LoginUserResponse>
+  findUserByEmail(email: string): Promise<UserProps | null>
 }
