483186d40044c7f885e7b3962ddaf186e6f05559
Matheus <maatheus.santos223@gmail.com>
2023-11-16 14:12:19 -0300

    feat: add login-user to controller

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,9 +1,9 @@
-/* eslint-disable @typescript-eslint/ban-types */
 import { Request, Response } from 'express';
 import { CreateUserRequest } from '../DTO/user-dtos';
 import { DbUserRepository } from '../repositories/db-user-repository';
 import { CreateUser } from '../use-cases/users/create-user';
 import { randomUUID } from 'node:crypto';
+import { LoginUser, LoginUserRequest } from '../use-cases/users/login-user';
 
 export class UserController {
 
@@ -23,11 +23,33 @@ export class UserController {
     });
 
     if (result.isLeft()) {
-      response.status(result.value.statusCode).json(result);
+      response.status(result.value.statusCode).json(result.value.message);
       return;
     }
 
     response.status(201).json({ message: 'User created successfully' });
     return;
+  }
+
+  static async login(request: Request<{}, {}, LoginUserRequest>, response: Response) {
+
+    const { email, password } = request.body;
+
+    const repository = new DbUserRepository();
+    const loginUser = new LoginUser(repository);
+
+    const result = await loginUser.execute({
+      email,
+      password
+    });
+
+    if (result.isLeft()) {
+      response.status(result.value.statusCode).json(result.value.message);
+      return;
+    }
+
+    response.status(201).json(result.value);
+    return;
+
   }
 }
