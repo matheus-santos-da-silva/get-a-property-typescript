753af2427a8bfc0ad10b21a62ba440ac1ec82201
Matheus <maatheus.santos223@gmail.com>
2023-11-18 14:54:13 -0300

    test: changing user class test

--- src/entities/user.spec.ts
+++ src/entities/user.spec.ts
@@ -16,18 +16,4 @@ describe('Testing User', () => {
     expect(user).toBeInstanceOf(User);
     expect(user.name).toBe('John Doe');
   });
-
-  it('should not able to create a user if a password have not 5 characters at least', () => {
-
-    expect(() => {
-      new User({
-        id: '1111111',
-        email: 'test@email.com',
-        name: 'John Doe',
-        password: '123',
-        phone: '999999999'
-      });
-    }).toThrow();
-  });
-
 });
