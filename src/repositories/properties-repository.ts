44f8911855f1d115cf44d236bcdbd92393a08cfb
Matheus <maatheus.santos223@gmail.com>
2023-11-23 12:31:59 -0300

    feat: add add get-all-properties

--- src/repositories/properties-repository.ts
+++ src/repositories/properties-repository.ts
@@ -1,5 +1,7 @@
+import { getAllPropertiesProps } from '../DTO/property-dtos';
 import { Property } from '../entities/property';
 
 export interface PropertiesRepository {
   create(props: Property): Promise<void>
+  getAllProperties(): Promise<getAllPropertiesProps[]>
 }
