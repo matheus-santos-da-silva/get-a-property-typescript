a7011ff4a59dd814e9de0fa1d0348b67a88202e9
Matheus <maatheus.santos223@gmail.com>
2023-12-05 13:56:00 -0300

    feat: add Owner and ScheduleRepositoryProps DTOS

--- src/DTO/property-dtos.ts
+++ src/DTO/property-dtos.ts
@@ -1,4 +1,5 @@
 import { Decimal } from '@prisma/client/runtime/library';
+import { ContractorProps } from './contractor.dtos';
 
 export interface PropertyProps {
   id: string;
@@ -8,6 +9,20 @@ export interface PropertyProps {
   price: Decimal;
   description: string;
   available: boolean;
-  contractor?: string[]
   images?: string[];
+  id_user: string
+  contractorId: string | null
+}
+
+export interface Owner {
+  id: string
+  name: string
+  email: string
+  phone: string
+}
+
+export interface ScheduleRepositoryRequestProps {
+  propertyId: string
+  contractor: ContractorProps,
+  owner: Owner
 }
