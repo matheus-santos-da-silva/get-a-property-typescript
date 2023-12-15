81c12cb035768c307fee3a9ca09dd118112f66f7
Matheus Santos <maatheus.santos223@gmail.com>
2023-12-15 13:48:09 -0300

    fix: change errors status code

--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -10,12 +10,13 @@ export const validationMiddleware = <T>(
   return (request: Request, response: Response, next: NextFunction) => {
     try {
       const data: T = request.body;
+      console.log(data);
       schema.parse(data);
 
       next();
     } catch (error) {
       if (error instanceof z.ZodError) {
-        response.status(400).json(error.errors.map((error) => error.message));
+        response.status(422).json(error.errors.map((error) => error.message));
         return;
       }
     }
