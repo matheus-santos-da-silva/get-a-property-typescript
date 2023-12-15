4124f75f9b738b81ee6c53e3fff65684bc6f618c
Matheus Santos <maatheus.santos223@gmail.com>
2023-12-15 11:47:17 -0300

    fix: modifying the functions return

--- src/repositories/contractor-repository.ts
+++ src/repositories/contractor-repository.ts
@@ -4,6 +4,6 @@ import { PropertyProps } from '../DTO/property-dtos';
 export interface ContractorsRepository {
   create(contractor: Contractor, property: PropertyProps): Promise<void>
   getAllContractors(): Promise<ContractorProps[]>
-  checkIfVisitAlreadyScheduled(contractorId: string, propertyId: string): Promise<ContractorProps | null>
-  checkIfContractorExists(name: string, phone: string): Promise<ContractorProps | null>
+  checkIfVisitAlreadyScheduled(contractorId: string, propertyId: string): Promise<ContractorProps | Contractor | null>
+  checkIfContractorExists(name: string, phone: string): Promise<ContractorProps | Contractor | null>
 }
