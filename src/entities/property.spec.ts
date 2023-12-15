00da50e0cfdfe739d48474b60130a786be02f650
Matheus Santos <maatheus.santos223@gmail.com>
2023-12-15 11:35:02 -0300

    test: create a property fixed

--- src/entities/property.spec.ts
+++ src/entities/property.spec.ts
@@ -1,4 +1,5 @@
 import { describe, expect, it } from 'vitest';
+import { Decimal } from '@prisma/client/runtime/library';
 import { Property } from './property';
 
 describe('Propertiy class', () => {
@@ -9,10 +10,12 @@ describe('Propertiy class', () => {
       id: '11111111',
       category: 'test',
       title: 'test',
-      price: 10,
+      price: new Decimal(10),
       address: 'test',
       available: true,
       description: '',
+      contractorId: null,
+      id_user: 'id',
       user: {
         connect: {
           id: 'id'
