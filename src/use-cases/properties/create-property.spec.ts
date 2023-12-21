7d8e041588ca9af1e0d65214ea06b7a51653e4a6
Matheus Santos <maatheus.santos223@gmail.com>
2023-12-21 14:08:42 -0300

    fix: create-property tests fixed

--- src/use-cases/properties/create-property.spec.ts
+++ src/use-cases/properties/create-property.spec.ts
@@ -2,7 +2,6 @@ import { describe, expect, it } from 'vitest';
 import { InMemoryPropertiesRepository } from '../../repositories/in-memory/in-memory-properties-repository';
 import { CreateProperty } from './create-property';
 import { Property } from '../../entities/property';
-import { RequiredParametersError } from '../../errors/required-parameters-error';
 import { Decimal } from '@prisma/client/runtime/library';
 
 describe('Create Property', () => {
@@ -24,26 +23,6 @@ describe('Create Property', () => {
     });
 
     expect(result.value).toBeInstanceOf(Property);
-
-  });
-
-  it('should not to be able to create a new property if available is false', async () => {
-
-    const repository = new InMemoryPropertiesRepository();
-    const createProperty = new CreateProperty(repository);
-
-    const result = await createProperty.execute({
-      id: '11111',
-      address: 'test 123',
-      category: 'test',
-      description: 'test',
-      images: [],
-      price: new Decimal(123),
-      title: 'test',
-      user: { connect: { id: 'string' } }
-    });
-
-    expect(result.value).toBeInstanceOf(RequiredParametersError);
 
   });
 });
