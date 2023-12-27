31f24b566ff13cf80ca2a46328c7918158b99132
Matheus Santos <maatheus.santos223@gmail.com>
2023-12-27 15:09:45 -0300

    test: add address duplicated test to the create-property tests

--- src/use-cases/properties/create-property.spec.ts
+++ src/use-cases/properties/create-property.spec.ts
@@ -3,6 +3,8 @@ import { InMemoryPropertiesRepository } from '../../repositories/in-memory/in-me
 import { CreateProperty } from './create-property';
 import { Property } from '../../entities/property';
 import { Decimal } from '@prisma/client/runtime/library';
+import { mockProperty } from '../../mocks/mocks';
+import { RequiredParametersError } from '../../errors/required-parameters-error';
 
 describe('Create Property', () => {
 
@@ -23,6 +25,29 @@ describe('Create Property', () => {
     });
 
     expect(result.value).toBeInstanceOf(Property);
+
+  });
+
+  it('should not to be able to create a property if the address is already in use', async () => {
+
+    const repository = new InMemoryPropertiesRepository();
+    const createProperty = new CreateProperty(repository);
+
+    await createProperty.execute(mockProperty);
+
+    const result = await createProperty.execute({
+      id: '11111',
+      address: 'test 123',
+      category: 'test',
+      description: 'test',
+      images: [],
+      price: new Decimal(123),
+      title: 'test',
+      user: { connect: { id: 'string' } }
+    });
+
+    expect(result.value).toBeInstanceOf(RequiredParametersError);
+    expect(result.value).toContain({ _message: 'This address is already in use' });
 
   });
 });
