e200355e6b9cc3a178b7d4a3ca8881a53eb74b04
Matheus Santos <maatheus.santos223@gmail.com>
2024-01-03 14:40:48 -0300

    refactor: add mocks to get-all-users tests

--- src/use-cases/users/get-all-users.spec.ts
+++ src/use-cases/users/get-all-users.spec.ts
@@ -2,22 +2,7 @@ import { describe, expect, it } from 'vitest';
 import { InMemoryUsersRepository } from '../../repositories/in-memory/in-memory-users-repository';
 import { GetAllUsers } from './get-all-users';
 import { CreateUser } from './create-user';
-
-const mockUser1 = {
-  id: '1111111',
-  email: 'test@test.com',
-  name: 'test',
-  password: '123456',
-  phone: '111111111'
-};
-
-const mockUser2 = {
-  id: '2222222',
-  email: 'test2@test.com',
-  name: 'test',
-  password: '123456',
-  phone: '111111111'
-};
+import { mockUser, mockUser1 } from '../../mocks/mocks';
 
 describe('GetAllUsers', () => {
 
@@ -26,8 +11,8 @@ describe('GetAllUsers', () => {
     const repository = new InMemoryUsersRepository();
 
     const createUser = new CreateUser(repository);
+    await createUser.execute(mockUser);
     await createUser.execute(mockUser1);
-    await createUser.execute(mockUser2);
 
     const sut = new GetAllUsers(repository);
 
